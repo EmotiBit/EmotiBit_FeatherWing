fd2b4ad8900ff60cba09e156b56d4f8972d6502e
Nitin Nair <nitin7nair@gmail.com>
2020-04-02 14:45:38 -0400

    version bump to v1.0.58

--- EmotiBit.h
+++ EmotiBit.h
@@ -34,7 +34,7 @@ public:
 		length
 	};
 
-	String firmware_version = "1.0.57";
+	String firmware_version = "1.0.58";
 	TestingMode testingMode = TestingMode::NONE;
 	const bool DIGITAL_WRITE_DEBUG = false;
 
