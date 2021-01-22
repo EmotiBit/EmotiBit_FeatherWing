a48666a5386851be0f4f03f85c6f597e75c13b5b
Nitin Nair <nitin7nair@gmail.com>
2021-01-22 13:35:33 -0500

    Version bump to 1.2.5. Verfified working on V02h

--- EmotiBit.h
+++ EmotiBit.h
@@ -36,7 +36,7 @@ public:
 		length
 	};
 
-	String firmware_version = "1.2.4";
+	String firmware_version = "1.2.5";
 	TestingMode testingMode = TestingMode::NONE;
 	const bool DIGITAL_WRITE_DEBUG = false;
 
