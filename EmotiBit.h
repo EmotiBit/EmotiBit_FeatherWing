8852c5d91b81a9ae0cbdc084daf18db0b3105b56
Nitin Nair <nitin7nair@gmail.com>
2022-03-07 16:26:56 -0500

    stylistic change to SCR amplitude detection

--- EmotiBit.h
+++ EmotiBit.h
@@ -44,7 +44,7 @@ public:
 		length
 	};
 
-	String firmware_version = "1.3.28";
+	String firmware_version = "1.3.29";
 	TestingMode testingMode = TestingMode::NONE;
 	const bool DIGITAL_WRITE_DEBUG = false;
 	const bool DC_DO_V2 = true;
