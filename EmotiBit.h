70efe42d82846157c0a644033835ce0cc04e0b35
Nitin Nair <nitin7nair@gmail.com>
2022-06-23 12:37:18 -0400

    version update

--- EmotiBit.h
+++ EmotiBit.h
@@ -49,7 +49,7 @@ public:
 		length
 	};
 
-	String firmware_version = "1.3.36";
+	String firmware_version = "1.3.36.feat-Esp.0";
 
 	TestingMode testingMode = TestingMode::NONE;
 	const bool DIGITAL_WRITE_DEBUG = false;
