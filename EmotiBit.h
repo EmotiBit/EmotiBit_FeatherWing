d1fe5eb48944847470d1f726b9f658053994d0db
Nitin Nair <nitin7nair@gmail.com>
2022-06-30 15:21:44 -0400

    version bump

--- EmotiBit.h
+++ EmotiBit.h
@@ -48,7 +48,7 @@ public:
 		length
 	};
 
-	String firmware_version = "1.3.36.feat-Esp.3.feat-sleep.3";
+	String firmware_version = "1.3.36.feat-Esp.4";
 
 	TestingMode testingMode = TestingMode::NONE;
 	const bool DIGITAL_WRITE_DEBUG = false;
