a987d3180e919a1c9de6b02ecee6a74262d6e104
Sean Montgomery <produceconsumerobot@gmail.com>
2022-07-18 18:45:51 -0700

    bumped version

--- EmotiBit.h
+++ EmotiBit.h
@@ -48,7 +48,7 @@ public:
 		length
 	};
 
-  String firmware_version = "1.3.36.feat-Esp.7";
+  String firmware_version = "1.3.36.feat-Esp.7.feat-fixEspIfDef.0";
 
 	TestingMode testingMode = TestingMode::CHRONIC;
 	const bool DIGITAL_WRITE_DEBUG = true;
